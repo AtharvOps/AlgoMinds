e788aff90332fc677b51dabd1bded1c02860f690
varadgupta <varadgupta1208@gmail.com>
2026-04-04 03:12:02 +0530

    testing model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from api.v1.endpoints.predict import router
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "API is running 🚀"}
-
-app.include_router(router)
+    return {"message": "API running 🚀"}
